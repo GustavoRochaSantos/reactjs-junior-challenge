43eec317387fb920c23ddea5fa3bb4d5db49cf80
unknown <claricepassos@gmail.com>
2022-06-04 19:34:24 -0300

    search box and button made

--- src/components/spreadsheet.tsx
+++ src/components/spreadsheet.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import styled from 'styled-components'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
-function Spreadsheet (){
+const Conteiner = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center ;
+justify-content:  center;
+padding: 10px ;
+`
+
+const StyledStack = styled(Stack)`
+float: right: ;
+`
+
+
+
+
+function Spreadsheet() {
 
     return (
-        <div>Spreadsheet</div>
+        <Conteiner>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: 1, width: '500px'},
+                    }}
+                    noValidate
+                    autoComplete="off">
+                    <TextField id="outlined-basic" variant="outlined"  sx={{
+                        '& > :not(style)': { height: '40px' },
+                    }}/>
+
+                </Box>
+                <Stack spacing={2} direction="row" marginLeft={80} marginTop={-6}>
+                    <Button variant="outlined" >Buscar</Button>
+                </Stack>
+        </Conteiner>
+
     )
 
 }
