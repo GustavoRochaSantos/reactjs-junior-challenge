bfc36235551502deed6becc43b951075c0f2e6bc
franSborges <borgesfran988@gmail.com>
2022-06-03 21:57:38 -0300

    structuring project

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+
+function Footer() {
+  return (
+    <div className='content-footer'>
+      <div>hello world</div>
+      <div>hello world</div>
+    </div>
+    
+  )
+}
+
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import './index.css';
+import Logo from '../../assets/logo-topo 1.svg';
+
+
+function Header() {
+
+
+  return (
+    <header>
+      <div className="content-header">
+          <img src={Logo} alt="logo" />
+      </div>
+        
+        <div className='content-title'>
+          <h1>FrontEnd Challenge</h1>
+        </div>
+      
+    </header>
+    
+  )
+}
+
+export default Header;
