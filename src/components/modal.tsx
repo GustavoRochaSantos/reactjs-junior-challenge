8e5be038e1a0c4d93fbc791f93669a301ff39828
unknown <claricepassos@gmail.com>
2022-06-11 00:14:50 -0300

    form input correction

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -6,6 +6,7 @@ import { Button } from '@mui/material';
 import { v4 } from 'uuid';
 import axios from 'axios';
 import { useState } from 'react';
+import useForm from '../hooks/useForm';
 
 const ModalConteiner = styled.div`
 display: flex ;
@@ -14,143 +15,144 @@ align-items: center ;
 justify-content: center ;
 `
 
-interface ClientInfo{
-id: string,
-name: string,
-company: string,
-email: string,
-phone: string,
-address: string,
-note: string,
-isActive: boolean
+interface ClientInfo {
+    id: string,
+    name: string,
+    company: string,
+    email: string,
+    phone?: string,
+    address?: string,
+    note?: string,
+    isActive?: boolean
 }
 
 
-export class IdGenerator {
+/* export class IdGenerator {
     // constructor(){}
     generateId = (): string => v4()
-  }
+}
 
-  const idV4: string = new IdGenerator().generateId()
-
+const idV4: string = new IdGenerator().generateId()
+ */
 
 export default function Modal() {
- const [form, setForm] = useState<ClientInfo>({
-    id: idV4, 
-    name: "",
-    company: "", 
-    email: "", 
-    phone: "",
-    address: "",
-    note: "",
-    isActive: true
+    const [form, setForm] = useState<ClientInfo>({
+        id: v4(),
+        name: "",
+        company: "",
+        email: "",
+        phone: "",
+        address: "",
+        note: "",
+        isActive: true
     })
 
+    const createPost = (body: any) => {
 
-
-    const body = {
-        id: form.id,
-        name: form.name,
-        company: form.company,
-        email: form.email,
-        phone: form.phone,
-        adress: form.address,
-        note: form.note,
-        isActive: form.isActive
-    }
-
-    axios.post(
-        "http://localhost:3001/clients", body, {
+        axios.post(
+            "http://localhost:3001/clients", body, {
             headers: {
                 "Content-Type": "application/json",
             }
         })
-        .then((res) => {
-            setForm(res.data)
-            alert('Usuário cadastrado com sucesso')
-        })
-        .catch((err) => {
-            console.log("erro", err.response)
-        });
+            .then((res) => {
+                setForm(res.data)
+                console.log(form)
+                cleanFields()
+                alert('Usuário cadastrado com sucesso')
+            })
+            .catch((err) => {
+                console.log("erro", err.response.message)
+            });
+    }
 
-        const handleChange = (event: React.ChangeEvent<any>): void => {
-            setForm(event.target.value);
-          };
+    const onChange = (event: React.ChangeEvent<any>): void => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
 
-   const cleanFields = () => {
-        setForm({id:"", name:"", company:"", email:"",phone:"", address:"", note:"", isActive: true });
-      };
+    const cleanFields = () => {
+        setForm({ id: "", name: "", company: "", email: "", phone: "", address: "", note: "", isActive: true });
+    };
 
-    const sendForm = (event: React.FormEvent<any>) => {
+ const onSubmitForm = (event: React.FormEvent<any>) => {
         event.preventDefault()
-        console.log("Formulario enviado", form)
+        createPost(form)
         cleanFields()
-  } 
+    } 
+
+    console.log(form)
 
 
     return (
         <ModalConteiner>
-           <h2>Dados do cliente</h2>
-             <hr />
-    <Box
+            <h2>Dados do cliente</h2>
+            <hr />
+            <Box
                 component="form"
                 sx={{
                     '& .MuiTextField-root': { m: 1, width: '70ch' },
                 }}
                 noValidate
                 autoComplete="off"
-                onSubmit={sendForm}
-                  >
-                    <TextField
-                        id="outlined-multiline-flexible"
-                        label="Nome"
-                        value={form.name} 
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        id="outlined-textarea"
-                        label="Empresa"
-                        value={form.company}
-                        onChange={handleChange}
-                        multiline
-                        required
-                    />
-                    <TextField
-                        id="outlined-multiline-flexible"
-                        label="Telefone"
-                        maxRows={4}
-                        value={form.phone}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        id="outlined-textarea"
-                        label="Email"
-                        value={form.email}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        id="outlined-textarea"
-                        label="Endereço"
-                        value={form.address}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="Notas"
-                        multiline
-                        rows={4}
-                        value={form.note}
-                        onChange={handleChange}
-                        required
-                    />
-            </Box> 
-            
-            <Button variant="outlined" size='small'type="submit" >Adicionar</Button>
- 
+                onSubmit={onSubmitForm}
+            >
+                <TextField
+                    id="outlined-multiline-flexible"
+                    name="name"
+                    label="Nome"
+                    value={form.name}
+                    onChange={onChange}
+                    type='text'
+                    required
+                />
+                <TextField
+                    id="outlined-textarea"
+                    name="company"
+                    label="Empresa"
+                    value={form.company}
+                    onChange={onChange}
+                    type='text'
+                    required
+                />
+                <TextField
+                    id="outlined-multiline-flexible"
+                    name="phone"
+                    label="Telefone"
+                    value={form.phone}
+                    onChange={onChange}
+                    type='text'
+                    required
+                />
+                <TextField
+                    id="outlined-textarea"
+                    name="email"
+                    label="Email"
+                    value={form.email}
+                    onChange={onChange}
+                    type='email'
+                    required
+                />
+                <TextField
+                    id="outlined-textarea"
+                    name="address"
+                    label="Endereço"
+                    value={form.address}
+                    onChange={onChange}
+
+                />
+                <TextField
+                    id="outlined-multiline-static"
+                    name="note"
+                    label="Notas"
+                    multiline
+                    rows={4}
+                    value={form.note}
+                    onChange={onChange}
+                />
+                <Button variant="outlined" size='small' >Adicionar</Button>
+            </Box>
+
+
         </ModalConteiner>
     );
 }
--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-const useForm = (initialState: { guid: string; name: string; company: string; email: string; phone: string; address: string; note: string; isActive: boolean; }) =>{
+const useForm = (initialState: any) =>{
     const [form, setForm] = useState(initialState)
 
-    const handleInputChange = (event: { target: { value: any; name: any; }; }) =>{
+    const handleInputChange = (event: any) =>{ 
         const{value, name} = event.target
         setForm({...form, [name]: value})
     }
@@ -12,8 +12,9 @@ const useForm = (initialState: { guid: string; name: string; company: string; em
         setForm(initialState)
     }
 
-    return [form, handleInputChange, clear]
+    return [form, setForm, handleInputChange, clear]
 }
 
 export default useForm
+
 
